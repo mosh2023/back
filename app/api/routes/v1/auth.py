8f01148ebc10b2e53a13467d5c89917c3d0bcb98
Niumandzi <amazheykin@gmail.com>
2024-01-21 19:08:21 +0300

    fixed settings

--- app/api/routes/v1/auth.py
+++ app/api/routes/v1/auth.py
@@ -10,15 +10,14 @@ from passlib.context import CryptContext
 
 from app.core.settings import AppSettings
 from app.db.repository import AuthRepository
-from app.models.api import Id, Roles, Token, AuthInfo, AuthRequest, AuthModel
+from app.models.api import Id, Token, AuthInfo, AuthModel
 
 router = APIRouter(
     prefix="/v1", tags=['auth']
 )
 
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+
+
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -53,7 +52,7 @@ async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,
     print(auth_data.model_dump())
     print(type(auth_data.model_dump()))
 
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token_expires = timedelta(minutes=AppSettings.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(data={"sub": auth_data.model_dump()}, expires_delta=access_token_expires)
 
     return Token(access_token=access_token, token_type="bearer")
@@ -70,5 +69,5 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     else:
         expire = datetime.now(timezone.utc) + timedelta(minutes=15)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, AppSettings.SECRET_KEY, algorithm=AppSettings.ALGORITHM)
     return encoded_jwt
