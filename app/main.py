3b9cbe6e013077817aab50b4dd10a8d1e0ef6d24
Niumandzi <amazheykin@gmail.com>
2024-01-28 14:47:26 +0300

    fix cors

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ def get_application() -> FastAPI:
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost", "http://127.0.0.1"],
+        allow_origins=["http://localhost", "http://127.0.0.1", "http://localhost:3000"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
